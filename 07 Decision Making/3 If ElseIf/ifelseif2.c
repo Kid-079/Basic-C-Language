a8c99fec4bf8923257225539666dc23af0716c49
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-01-24 14:50:39 +0700

    Add files via upload

---
+++ 07 Decision Making/3 If ElseIf/ifelseif2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	int a = 4;
+
+	if (a == 0){
+		printf("Unprogress, System Failure\n");
+	} else if (a == 1) {
+		printf("Check 1\n");
+	} else if (a == 2) {
+		printf("Check 2\n");
+	} else if (a == 3){
+		printf("Check 3\n");
+	} else {
+		printf("Try Again.. Follow The Instructions.. \n");
+		printf("Check 1.. 2.. Check 1.. 2.. Check 1.. 2.. 3... \n");
+	}
+	
+	return 0;
+}
