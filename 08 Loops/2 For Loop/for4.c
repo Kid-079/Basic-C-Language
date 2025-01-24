80c05ae0ca680ed5b01a92f9025f4d1a845c0314
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-01-24 14:51:17 +0700

    Add files via upload

---
+++ 08 Loops/2 For Loop/for4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	int check = 0;
+
+	for (int check = 0; check < 10; check++){
+		print("Check Value : %d\n", check);
+		if (check == 1){
+			printf("-> Channel 1.. Check\n");
+			continue;
+		} else if (check == 6){
+			printf("-> Channel 2.. Check\n");
+			continue;
+		} else if (check == 9)(
+			printf("-> Channel 3.. Check\n");
+			continue;
+		)	
+		
+		printf("No Input Element.. Cannot Access The Channel\n");
+	}
+	return 0;
+}
