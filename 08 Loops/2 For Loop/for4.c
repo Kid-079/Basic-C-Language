d3b1eacd5e0aef8f878a683e9db1b777884e3f75
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-26 10:00:27 +0700

    Update for4.c

--- 08 Loops/2 For Loop/for4.c
+++ 08 Loops/2 For Loop/for4.c
@@ -6,17 +6,21 @@ int main(){
 	for (int check = 0; check < 10; check++){
 		print("Check Value : %d\n", check);
 		if (check == 1){
-			printf("-> Channel 1.. Check\n");
+			//printf("-> Channel 1.. Check\n");
+			printf("-> Beast\n");
 			continue;
 		} else if (check == 6){
-			printf("-> Channel 2.. Check\n");
+			//printf("-> Channel 2.. Check\n");
+			printf("-> Stronger\n");
 			continue;
 		} else if (check == 9)(
-			printf("-> Channel 3.. Check\n");
+			//printf("-> Channel 3.. Check\n");
+			printf("-> Cowboy\n");
 			continue;
 		)	
 		
-		printf("No Input Element.. Cannot Access The Channel\n");
+		//printf("No Input Element.. Cannot Access The Channel\n");
+		printf("Not Identified\n");
 	}
 	return 0;
 }
