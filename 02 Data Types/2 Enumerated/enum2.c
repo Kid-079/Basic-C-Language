8290cc017ef9b6f94895318298155678c0a74808
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-28 11:00:58 +0700

    Update enum2.c

--- 02 Data Types/2 Enumerated/enum2.c
+++ 02 Data Types/2 Enumerated/enum2.c
@@ -5,9 +5,10 @@ enum Year {Jan, Feb, March, Apr, May, Jun, Jul, Aug, Sept, Okt, Nov, Des};
 int main()
 {
 	enum Year Month ;
-	Month = Jun;
-	// printf("Month %d\n", Month);
-	printf("Month %d\n", Month+1);
+	Month = Jul;
+	printf("Month %d\n", Month);
+	//printf("Month %d\n", Month+1);
+	//printf("Month %d\n", Month-1);
 	
 	return 0;
 }
