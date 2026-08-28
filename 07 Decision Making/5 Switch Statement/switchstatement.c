606c46b5c5e7c7066f2a7fb7f45cc6dc44ec2a6e
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-28 10:33:09 +0700

    Update switchstatement.c

--- 07 Decision Making/5 Switch Statement/switchstatement.c
+++ 07 Decision Making/5 Switch Statement/switchstatement.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
 int main(){
-	char grade = 'E';
+	char grade = 'G';
 
 	switch(grade) {
 	case 'A' :
-		printf("Very Good\n");
+		printf("Wonderfull\n");
 		break;
 	case 'B' :
-		printf("Good\n");
+		printf("Very Good\n");
 		break;
 	case 'C' :
-		printf("Not Bad\n");
+		printf("Good\n");
 		break;
 	case 'D' :
+		printf("Not Bad\n");
+		break;
+	case 'E' :
 		printf("Bad\n");
+		break;
+	case 'F' :
+		printf("Really Bad\n");
 		break;
 	default :
 		printf("Invalid Grade\n");
